6f17774fed5fead35e09f1c772d68b4a2cdb52e8
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-06 22:40:37 +0530

    Optimization of program logic with file handling concepts in it

--- miscellaneous/substitution_cipher.py
+++ miscellaneous/substitution_cipher.py
@@ -14,27 +14,39 @@ key = chars.copy()
 
 random.shuffle(key)
 
-# print(f"chars: {chars}")
-# print(f"key: {key}")
+# Saving the key
+with open("key.txt", "w") as f:
+    f.write("".join(key))
 
-# Encryption
-msg = input("Enter a message to encrypt: ")
-cipher = ""
+with open("key.txt", "r") as f:
+    key = list(f.read())
 
-for letter in msg:
-    idx = chars.index(letter)
-    cipher += key[idx]
+encrypt_map = dict(zip(chars, key))
+decrypt_map = dict(zip(key, chars))
 
-print(f"Original message: {msg}")
-print(f"Encrypted message: {cipher}")
+print("1. Encrypt")
+print("2. Decrypt")
+choice = input("Choose: ")
 
-# Decryption
-cipher = input("Enter a message to decrypt: ")
-org_msg = ""
+# Encrypt
+if choice == "1":
+    msg = input("Enter message: ")
+    cipher = ""
 
-for letter in cipher:
-    idx = key.index(letter)
-    org_msg += chars[idx]
+    for letter in msg:
+        if letter in encrypt_map:
+            cipher += encrypt_map[letter]
+        else:
+            cipher += letter
+# Decrypt
+elif choice == "2":
+    cipher = input("Enter encrypted message: ")
+    org_msg = ""
 
-print(f"Encrypted message: {cipher}")
-print(f"Decrypted message: {org_msg}")
+    for letter in cipher:
+        if letter in decrypt_map:
+            org_msg += decrypt_map[letter]
+        else:
+            org_msg += letter
+
+print("Result: ",cipher)
