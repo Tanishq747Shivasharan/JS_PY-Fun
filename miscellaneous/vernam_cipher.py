d5ec3b58bb365133af8c8c9109be1df209dd974c
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-07 22:55:51 +0530

    Fully optimized the code with error handling for empty string.

--- miscellaneous/vernam_cipher.py
+++ miscellaneous/vernam_cipher.py
@@ -1,10 +1,15 @@
 import os
 import secrets
 
-plain_txt = input('Enter a message: ')
+while True:
+    plain_txt = input('Enter a message: ')
+    if not plain_txt:
+        print("Empty message - nothing to encrypt.TRY AGAIN...")
+    else:
+        break
 encodedPlain_txt = plain_txt.encode() # encoding the plain text to byte.
 
-key = secrets.token_bytes(len(plain_txt)) # encoding the key to byte.
+key = secrets.token_bytes(len(encodedPlain_txt)) # encoding the key to byte.
 
 list_txt = list(encodedPlain_txt) # converting the encoded plain text to list.
 list_key = list(key) # converting the key to list.
@@ -19,8 +24,8 @@ print('Encrypted message:', ciphertext_bytes.hex())
 
 # Decryption
 for i in range(len(ciphertext_bytes)):
-    xor_res = list_txt[i] ^ list_key[i] # XOR operation to encrypt the plain text.
+    xor_res = list_txt[i] ^ list_key[i] # XOR operation to decrypt the plain text.
     list_txt[i] = xor_res
     
 decrypted_bytes = bytes(list_txt)
-print('Decrypted message:', ciphertext_bytes.decode())
+print('Decrypted message:', decrypted_bytes.decode())
