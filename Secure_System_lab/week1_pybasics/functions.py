419d0a05817bf014f47b74c8713134a86bec9e75
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-15 22:45:47 +0530

    Types of Arguments in Python.

--- Secure_System_lab/week1_pybasics/functions.py
+++ Secure_System_lab/week1_pybasics/functions.py
@@ -18,3 +18,36 @@ if __name__ == "__main__":
 #     greet(name)
 #     ^^^^^
 # NameError: name 'greet' is not defined
+
+# calling a function.
+print(greet("Manya"))
+
+# function arguments.
+# 1. Default arguments
+# 2. Keyword arguments
+# 3. Positional arguments
+# 4. Arbitrary arguments
+
+def greetWithDefault_age(name, age = 18):   # --> Default argument is age assigned with the value 18.
+    print(f"Hello {name}! You are {age} years old.")
+
+# static way of calling a function.
+greetWithDefault_age("Tanishq")
+greetWithDefault_age("Manya", 20) # --> No error displayed here the assigned value in the arguments given is assigned with new value that is 20.
+
+def greetWithKeyword_arg(fname, lname): # --> keyword arguments are both fname and lname.
+    print(f"Hello {fname} {lname}!")
+    
+# static way of calling a function.
+greetWithKeyword_arg(fname = "Tanishq", lname = "Shivasharan") # --> Keyword arguments are used to assign the values to the arguments in the function.
+greetWithKeyword_arg(lname = "Gundla", lname = "Manya")
+
+def nameAge(name, age): # --> Positional arguments are name and age values are assigned to parameters based on their order in the function call.
+    print("Hi, I am", name)
+    print("My age is ", age)
+
+print("Case-1:")
+nameAge("Suraj", 27)
+
+print("\nCase-2:")
+nameAge(27, "Suraj")
