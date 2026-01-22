56204c014a4a15685e9bfbcf5f1d470e49e7a081
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-22 22:11:06 +0530

    Created an Caesar Cipher(Cryptograhic) program.

---
+++ miscellaneous/Caesar_Cipher.py
@@ -0,0 +1,37 @@
+""" 
+1. Ask the user to enter a message.
+2. Ask the user for a shift number.
+3. For each letter in the message:
+     - Shift it by the given number.
+     - Wrap around if it goes past 'Z' or 'z'.
+4. Print the encrypted message. """
+
+u_msg = input("Enter your message: ") 
+u_num = int(input("Enter a shift number: "))
+
+# ord('A') converts the character 'A' to its ASCII value that is 65.
+# chr(68) converts the ASCII value 68 to its character that is 'D'.
+
+# Encryption
+encrypted = ''
+for ch in u_msg:
+    if ch.isalpha():
+        if ch.isupper():
+            encrypted = chr(((ord(ch) - ord('A') + u_num) % 26) + ord('A')), end=''
+        else:
+            encrypted = chr(((ord(ch) - ord('a') + u_num) % 26) + ord('a')), end=''
+    else:        
+        print(ch, end='')
+        print(f"Encrypted message: {encrypted}", end='')
+
+# Decryption
+decrypted = ''
+for ch in encrypted
+    if ch.isalpha():
+        if ch.isupper():
+            decrypted = chr(((ord(ch) - ord('A') - u_num) % 26) + ord('A')), end=''
+        else:
+            decrypted = chr(((ord(ch) - ord('a') - u_num) % 26) + ord('a')), end=''
+    else:
+        print(ch, end='')
+        print(f"Decrypted message: {decrypted}", end='')
