c6ee24df9d51f47486dd362c677ba7df6eb91332
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-27 22:42:08 +0530

    Code optimization and user input/output handling and improvements.

--- Secure_System_lab/week1_pybasics/lists_dicts.py
+++ Secure_System_lab/week1_pybasics/lists_dicts.py
@@ -1,17 +1,35 @@
 # list in python--> mutable, ordered, index-based, allows mixed datatypes, can contain duplicate items.
 users = ['tanishq123', 'rahul_kumar', 'ankit99', 'priya_singh', 'chatGPT', 'alex', 'user2020']
 
-find_user = input("Enter user to find: ")
+while True:
+    print("----------------------------------------------------------")
+    print("\nEnter `quit` for exiting the program")
+    print("Enter `add` for adding new uesr to list")
+    print("Enter `ls` for viewing the full list of usernames")
+    print("Enter `find` for searching a specific user from the list")
+    print("----------------------------------------------------------")
+    
+    user_cmd = input("Enter your choice: ")
 
-if find_user in users:
-    print(f"User {find_user} found at index {users.index(find_user)}")
-else:
-    print(f"User {find_user} doesn't exists!")
-    if find_user not in users:
-        users.append(find_user)
-    print(f"User {find_user} added successfully!")
-
-print(users)
+    if user_cmd == 'add':
+        new_user = input("Enter the new username: ")
+        users.append(new_user)
+        print(f"User {new_user} added successfully!")
+        print(users)
+    elif user_cmd == 'ls':
+        for i in users:
+            print(i)
+    elif user_cmd == 'find':
+        find_user = input("Enter the username you want to search: ")
+        if find_user in users:
+            print(f"User {find_user} found at index {users.index(find_user)}")
+        else:
+            print(f"User {find_user} doesn't exists!")
+    elif user_cmd == 'quit':
+        print("Process terminated!")
+        break
+    else:
+        print("Invalid input! Please try again.")
 
 # for find_user in users:
 #     if find_user is users[find_user]:
