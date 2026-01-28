1cebb1c5c923d4dcddc3cc75972b42ff0e2e0b47
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-28 23:40:52 +0530

    Dictionary access control.

--- Secure_System_lab/week1_pybasics/lists_dicts.py
+++ Secure_System_lab/week1_pybasics/lists_dicts.py
@@ -1,38 +1,44 @@
 # list in python--> mutable, ordered, index-based, allows mixed datatypes, can contain duplicate items.
-users = ['tanishq123', 'rahul_kumar', 'ankit99', 'priya_singh', 'chatGPT', 'alex', 'user2020']
+# users = ['tanishq123', 'rahul_kumar', 'ankit99', 'priya_singh', 'chatGPT', 'alex', 'user2020']
 
-while True:
-    print("----------------------------------------------------------")
-    print("\nEnter `quit` for exiting the program")
-    print("Enter `add` for adding new uesr to list")
-    print("Enter `ls` for viewing the full list of usernames")
-    print("Enter `find` for searching a specific user from the list")
-    print("----------------------------------------------------------")
+# while True:
+#     print("----------------------------------------------------------")
+#     print("\nEnter `quit` for exiting the program")
+#     print("Enter `add` for adding new uesr to list")
+#     print("Enter `ls` for viewing the full list of usernames")
+#     print("Enter `find` for searching a specific user from the list")
+#     print("----------------------------------------------------------")
     
-    user_cmd = input("Enter your choice: ")
+#     user_cmd = input("Enter your choice: ")
 
-    if user_cmd == 'add':
-        new_user = input("Enter the new username: ")
-        users.append(new_user)
-        print(f"User {new_user} added successfully!")
-        print(users)
-    elif user_cmd == 'ls':
-        for i in users:
-            print(i)
-    elif user_cmd == 'find':
-        find_user = input("Enter the username you want to search: ")
-        if find_user in users:
-            print(f"User {find_user} found at index {users.index(find_user)}")
-        else:
-            print(f"User {find_user} doesn't exists!")
-    elif user_cmd == 'quit':
-        print("Process terminated!")
-        break
-    else:
-        print("Invalid input! Please try again.")
-
-# for find_user in users:
-#     if find_user is users[find_user]:
-#         print(f"Ther user {users[user]} is present in the list.")
+#     if user_cmd == 'add':
+#         new_user = input("Enter the new username: ")
+#         users.append(new_user)
+#         print(f"User {new_user} added successfully!")
+#         print(users)
+#     elif user_cmd == 'ls':
+#         for i in users:
+#             print(i)
+#     elif user_cmd == 'find':
+#         find_user = input("Enter the username you want to search: ")
+#         if find_user in users:
+#             print(f"User {find_user} found at index {users.index(find_user)}")
+#         else:
+#             print(f"User {find_user} doesn't exists!")
+#     elif user_cmd == 'quit':
+#         print("Process terminated!")
+#         break
 #     else:
-#         print(f"The user doesn't exist in the database!")
+#         print("Invalid input! Please try again.")
+
+# Dictionary based authentication.
+users = {
+    "admin": "1234",
+    "tanishq": "pass"
+}
+
+for username, password in users.items():
+    if username == "admin" or username == "tanishq" and password == "1234" or password == "pass":
+        print("Access granted!")
+    else:
+        print("Access denied!")
