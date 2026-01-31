923e85b5c822113cbbde1560287bd82abaa3bb35
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-01-31 23:10:16 +0530

    Created an access logger using Python.

--- Secure_System_lab/week2_pybasics/logger.py
+++ Secure_System_lab/week2_pybasics/logger.py
@@ -1,3 +1,5 @@
+from datetime import datetime,timezone
+
 # Dictionary based authentication.
 users = {
     "admin" : "1234",
@@ -8,10 +10,15 @@ uname = input("Enter the username: ")
 pwd = input("Enter the password: ")
 
 if uname in users and users[uname] == pwd:
+    timestamp = datetime.now().strftime("%H:%M:%S")
     print("Access granted!")
+    with open("logs.txt", "a") as wf:
+        wf.write(f"Successfully logged in by {uname} at {timestamp}\n")
 else:
+    timestamp = datetime.now().strftime("%H:%M:%S")
     print("Access denied!")
     with open("logs.txt", "a") as wf:
         # Added \n for a new line
         wf.write(f"Failed login attempt for {uname}\n") 
+        wf.write(f"Failed attempt by {uname} at {timestamp}\n")
         print("User log activity saved in logs.txt")
