b43cf3bf0b5f0509405407958008f7e2555ed348
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-02-11 23:38:16 +0530

    Password filter.

--- miscellaneous/pwd_Strength_Checker.py
+++ miscellaneous/pwd_Strength_Checker.py
@@ -1,46 +1,59 @@
-import re
+import string
 
-def check_weak_passwd(passwd):
-    rules = [
-        {
-            "message": "Password must be at least 8 characters long",
-            "test": lambda pwd: len(pwd) >= 8
-        },
-        {
-            "message": "Password must contain an uppercase letter",
-            "test": lambda pwd: bool(re.search(r"[A-Z]", pwd))
-        },
-        {
-            "message": "Password must contain a lowercase letter",
-            "test": lambda pwd: bool(re.search(r"[a-z]", pwd))
-        },
-        {
-            "message": "Password must contain a number",
-            "test": lambda pwd: bool(re.search(r"[0-9]", pwd))
-        },
-        {
-            "message": "Password must contain a special character",
-            "test": lambda pwd: bool(re.search(r"[@$!%*?&]", pwd))
-        }
-    ]
+def check_password_strength(passwd):
+    score = 0
+    sugg = []
 
-    failed_rules = list(filter(lambda rule: not rule["test"](passwd), rules))
+    if len(passwd) >= 8:
+        score += 1
+    else:
+        sugg.append("Make password at least 8 characters long")
 
-    return [rule["message"] for rule in failed_rules] 
+    if any(char.isupper() for char in passwd):
+        score += 1
+    else:
+        sugg.append("Add at least one uppercase letter")
 
+    if any(char.isdigit() for char in passwd):
+        score += 1
+    else:
+        sugg.append("Include at least one number")
+
+    if any(char in string.punctuation for char in passwd):
+        score += 1
+    else:
+        sugg.append("Include at least one number")
+
+    if any(char in string.punctuation for char in passwd):
+        score += 1
+    else:
+        sugg.append("Add at least one number")
+
+    if any(char in string.punctuation for char in passwd):
+        score += 1
+    else:
+        sugg.append("Add at least one special symbol {!@#$ etc.}")
+
+        # Strength Rating
+    if score == 4:
+        strength = "Very Strong"
+    elif score == 3:
+        strength = "Strong"
+    elif score == 2:
+        strength = "Medium"
+    else:
+        strength = "Weak"
+
+    return strength, sugg
 
 passwd = input("Enter your password: ")
 
-errs = check_weak_passwd(passwd)
-if len(errs) == 0:
-    print("Your password is strong enough")  
+strength, sugge = check_password_strength(passwd)
+print("\nPassword Strength:", strength)
+
+if sugge:
+    print("Suggestions to improve:")
+    for s in sugge:
+        print("-", s)
 else:
-    print("Your password is weak, please try again")
-    print("Your password should contain at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 number and 1 special character")
-    print("Your password should not contain any spaces")
-    print("Your password should not contain any common words")
-    print("Your password should not contain any common sequences")
-    print("Your password should not contain any common patterns")
-    print("Your password should not contain any common substrings")
-    for err in errs:
-        print("-",err)
+    print("No suggestions. Great password!")
