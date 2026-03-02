5659ef8392eb57a432521a7c69705d3843a3dbe5
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-03-02 23:38:49 +0530

    playing with os modules and methods in python.

--- Secure_System_lab/week3_pybasics/os_modules.py
+++ Secure_System_lab/week3_pybasics/os_modules.py
@@ -1,29 +1,67 @@
 import os
 
-current_dir = os.getcwd()
-print(current_dir)  # Prints the current Path of working directory.
+def show_current_directory():
+    print("Current Directory:", os.getcwd())
 
-change_dir = os.chdir("TEST_DIR")
-print("Changed directory into TEST_DIR") # Similar to cd command in Linux OS
+def list_files():
+    print("\nFiles and Folders:")
+    for item in os.listdir():
+        print("-", item)
 
-ls = os.listdir()
-print("The files in current directory are: ", ls) # Similar to the ls command in Linux OS
+def create_folder(folder_name):
+    try:
+        os.mkdir(folder_name)
+        print("Folder created successfully.")
+    except FileExistError:
+        print("Folder already exists.")
+    except Exception as e:
+        print("Error:",e)
 
-try:
-    os.mkdir("TEST_DIR2") # Make(Create) Directory similar to mkdir command in Linux OS
-    print(f"Successfully created the {mkdir} folder.")
-except FileExistsError:
-    print("The folder already exists")
-except Exception as e:
-    print("Error: ",e)
 
-os.remove("sample.txt") # not recommanded to execute as it will cause loss of all files contained in the current working directory.
+def delete_files(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        print("File deleted.")
+    else:
+        print("File not found.")
 
-# The professional way to handle things is using.
-if os.path.exists("sample.txt"):
-    os.remove("sample.txt")
-else:
-    print("The file does not exist")
+def rename_file(old_name, new_name):
+    if os.path.exists(old_name):
+        os.rename(old_name, new_name)
+        print("File renamed.")
+    else:
+        print("File not found.")
 
-# rename file using
-os.rename("sample.txt","demo.txt")
+def main():
+    while True:
+        print("\n--- Secure Directory Inspector ---")
+        print("1. Show Current Directory")
+        print("2. List Files")
+        print("3. Create folder")
+        print("4. Delete File")
+        print("5. Rename file")
+        print("6. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            show_current_directory()
+        elif choice == "2":
+            list_files()
+        elif choice == "3":
+            folder = input("Enter folder name: ")
+            create_folder(folder)
+        elif choice == "4":
+            file = input("Enter file name to delete: ")
+            delete_file(file)
+        elif choice == "5":
+            old = input("Old file name: ")
+            new = input("New file name: ")
+            rename_file(old, new)
+        elif choice == "6":
+            break
+        else:
+            print("Invalid choice.")
+
+if __name__ == "__main__":
+    main()
