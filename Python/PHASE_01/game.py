86a142426f912d42220e12cefebebbcd90d8e101
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-03-20 08:49:19 +0530

    Added rematch code with gracefull user message handling.

--- Python/PHASE_01/game.py
+++ Python/PHASE_01/game.py
@@ -46,8 +46,8 @@ def decide_winner(p1_choice, p2_choice):
 def play_game():
     p1, p2, rounds, mode = gameplay_setup()
 
-    score1 = 0
-    score2 = 0
+    p1_score1 = 0
+    p2_score2 = 0
 
     for i in range(rounds):
         print(f"\nRound {i+1}")
@@ -56,24 +56,38 @@ def play_game():
 
         if mode == 2:
             p2_choice = random.choice(elements)
-            print(f"{p2} chose: {p2_choice}")
+            print(f"{p2} choose: {p2_choice}")
         else:
             p2_choice = input(f"{p2} choose: ").lower()
 
         result = decide_winner(p1_choice, p2_choice)
 
         if result == "player1":
-            score1 += 1
+            p1_score1 += 1
             print(f"{p1} wins this round!")
         elif result == "player2":
-            score2 += 1
+            p2_score2 += 1
             print(f"{p2} wins this round!")
         else:
             print("Draw!")
 
     print("\n--- Final Result ---")
-    print(p1, ":", score1)
-    print(p2, ":", score2)
+    print(p1, ":", p1_score1)
+    print(p2, ":", p2_score2)
+
+    print("\n--- Winnner ---")
+    if p1_score1 > p2_score2:
+        print(f"{p1} wins!")
+    else:
+        print(f"{p2} wins!")
+
+    print("---Thanks for playing---")
+
+    restart = input("Would you like to play again? (y/n): ")
+    if restart == 'y':
+        play_game()
+    else:
+        print("See you soon!")
 
 if __name__ == '__main__':
     welcome()
