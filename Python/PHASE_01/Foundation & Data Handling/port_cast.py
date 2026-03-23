97bb354a7797909cb63efc265ce9e2ac8c31b1e7
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-03-23 15:26:45 +0530

    spell correction.

--- Python/PHASE_01/Foundation & Data Handling/port_cast.py
+++ Python/PHASE_01/Foundation & Data Handling/port_cast.py
@@ -1,6 +1,6 @@
 # Casting and Type Conversion
 
-port_num = int(input("Enter a randome port number: "))
+port_num = int(input("Enter a random port number: "))
 
 print(type(port_num))
 print(type(float(port_num)))
