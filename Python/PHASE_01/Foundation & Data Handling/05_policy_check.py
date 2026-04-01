325646065c4d425438246bd402e258d3065f28d0
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-04-01 21:46:35 +0530

    Some more basic knowledge(Foundation) about operators and logic.

--- Python/PHASE_01/Foundation & Data Handling/05_policy_check.py
+++ Python/PHASE_01/Foundation & Data Handling/05_policy_check.py
@@ -1,5 +1,14 @@
 # Operators & Logic
 
+# Operators Precedence in Python
+"""
+Python follows PEMDAS(Parentheses, Exponents, Multiplication, Division, Addtion, Subtraction)
+1. Parentheses () - Highest precedence, operations inside parentheses are evaluated first.
+2. Exponents ** - Power calculations (e.g. 2**3-->8) Evaluated from right to left.(Highest precedence)
+3. Multiplication *, Division /, Floor Division //, Modulus % - Evaluated from left to right.(Lower precedence)
+4. Addition +, Subtraction - - Evaulated from left to right.
+"""
+
 password = input("Enter a password: ")
 is_encrypted = True
 
