142d70ef3ba5f0fac113fbf0e3d40a4b19cc8ca7
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-01 20:07:25 +0530

    Once again playing with strings for better foundational code knowledge.

--- Python/PHASE_02/RECAP/01_log_string_parser.py
+++ Python/PHASE_02/RECAP/01_log_string_parser.py
@@ -1,1 +1,14 @@
 # Strings: Working with Textual Data
+
+sample_stringMsg = '2026-04-13 ERROR: Unauthorized access attempt at 192.168.1.1'
+
+# use this to see which all methods of string are present in python.
+# print(dir(sample_stringMsg))
+# also you can use this to get a thorough understanding of string methods/functions
+# print(help(str))
+
+error = sample_stringMsg[11: 16]
+print(error)
+
+ip = sample_stringMsg[49:]
+print(ip)
