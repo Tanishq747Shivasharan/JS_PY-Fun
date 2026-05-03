12c43cddf6a1d597a225d6888e58784341ef0c9c
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-03 12:00:00 +0530

    Server uptime calculator

--- Python/PHASE_02/RECAP/02_uptime_calc.py
+++ Python/PHASE_02/RECAP/02_uptime_calc.py
@@ -1,1 +1,8 @@
 # Integers and Floats: Numeric Security Data
+
+total_seconds = 345600
+uptime_hours = total_seconds/3600
+
+final_report = round(uptime_hours)
+
+print(f"Server uptime: {final_report} hours")
