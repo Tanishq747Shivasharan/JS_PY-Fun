ffae02226d80df64686de0619b7fca9b2d1ba306
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-05 23:32:08 +0530

    Port scanning with basic python code.

--- Python/PHASE_02/RECAP/04_port_map.py
+++ Python/PHASE_02/RECAP/04_port_map.py
@@ -1,1 +1,11 @@
 # Dictionaries: Mappping Services and Ports
+
+ip_Man = {
+    80 : 'http',
+    443: 'https',
+    22 : 'ssh'
+}
+
+user_port = int(input("Enter a port number: "))
+
+print(ip_Man.get(user_port, "service not found!"))
