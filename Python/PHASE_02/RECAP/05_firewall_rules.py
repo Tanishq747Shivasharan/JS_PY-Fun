c60a84f1fd3cf4dd8739645daef9b97755f0b9c6
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-08 23:55:57 +0530

    Conditionals and boolean code recap.

--- Python/PHASE_02/RECAP/05_firewall_rules.py
+++ Python/PHASE_02/RECAP/05_firewall_rules.py
@@ -1,1 +1,10 @@
 # Conditionals and Booleans: Access Control Logic
+
+user_status = 'Admin'
+
+login_time = int(input("Enter login time: "))
+
+if user_status == 'Admin' and 9 < login_time < 17:
+    print("Access Granted")
+else:
+    print("Access Denied")
