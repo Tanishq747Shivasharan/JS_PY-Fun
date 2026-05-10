aae6e1dc5fafc26e412b0f59300657610c117f02
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-10 22:52:52 +0530

    Port sweeping logic done.

--- Python/PHASE_02/RECAP/06_port_sweep.py
+++ Python/PHASE_02/RECAP/06_port_sweep.py
@@ -1,1 +1,8 @@
 # Loops and Iterations: Automated Port Sweeping
+
+ports = [80, 24, 120, 442, 200, 22, 69, 202, 404]
+
+for i in ports:
+    if i == 22:
+        print("Critical Service Found")
+        break
