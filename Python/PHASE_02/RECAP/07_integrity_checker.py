22484b15bbc28cd1c4cb9bddac13b17ed11236a9
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-11 09:03:14 +0530

    functions code implemented.

--- Python/PHASE_02/RECAP/07_integrity_checker.py
+++ Python/PHASE_02/RECAP/07_integrity_checker.py
@@ -1,1 +1,11 @@
 # Functions: Reusable Security Tools
+
+def validate_ip(ip):
+    parts = ip.split(".")
+    if len(parts) == 4:
+        return True
+    else:
+        return False
+
+ip = input("Enter any IPv4 address: ")
+print(validate_ip(ip))
