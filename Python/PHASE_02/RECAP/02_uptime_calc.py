4851cb5dd0e28888b4607d9cb98d6ad04e5aa3bc
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-05-12 12:00:00 +0530

    fundamentals day 3 practice.

--- Python/PHASE_02/RECAP/02_uptime_calc.py
+++ Python/PHASE_02/RECAP/02_uptime_calc.py
@@ -1,8 +1,30 @@
 # Integers and Floats: Numeric Security Data
 
-total_seconds = 345600
-uptime_hours = total_seconds/3600
+# total_seconds = 345600
+# uptime_hours = total_seconds/3600
 
-final_report = round(uptime_hours)
+# final_report = round(uptime_hours)
 
-print(f"Server uptime: {final_report} hours")
+# print(f"Server uptime: {final_report} hours")
+
+age = 18
+height = 6.0
+compx = 5+6j
+
+base = int(input("Enter base: "))
+height = int(input("Enter height: "))
+area = 0.5 * base * height
+print(f"The are of the triangle is {area}")
+
+a = int(input("Enter side a: "))
+b = int(input("Enter side b: "))
+c = int(input("Enter side c: "))
+perimeter = a + b + c
+print(f"The perimeter of the triangle is {perimeter}")
+
+length = float(input("Enter length of a rectangle: "))
+width = float(input("Enter width of a rectangle: "))
+area_of_rect = length * width
+perimeter_of_rect = 2 * (length + width)
+print(f"The area of rectangle is {area_of_rect}")
+print(f"The perimeter of rectangle is {perimeter_of_rect}")
