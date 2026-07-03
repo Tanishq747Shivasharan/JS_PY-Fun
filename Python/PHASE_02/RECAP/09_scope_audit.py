c9a8e6508d0812784418d2239feadaa7e250b861
Tanishq747Shivasharan <tanishqshivasharan747@gmail.com>
2026-07-03 12:00:00 +0530

    global keyword implementatio.

--- Python/PHASE_02/RECAP/09_scope_audit.py
+++ Python/PHASE_02/RECAP/09_scope_audit.py
@@ -1,1 +1,11 @@
 # Variable Scope: Managing Configuration Levels
+
+SECURITY_LEVEL = "High"
+
+def keyword_previlage():
+    global SECURITY_LEVEL 
+    SECURITY_LEVEL = "Critical"
+    print(SECURITY_LEVEL)
+
+print(SECURITY_LEVEL)
+keyword_previlage()
